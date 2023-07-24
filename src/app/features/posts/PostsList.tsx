5ea9605308e21b6f10352c42a988180b8f063a39
PavelM <myagkiypavel@gmail.com>
2023-07-24 18:10:39 +0300

    style: PostsList

--- src/app/features/posts/PostsList.tsx
+++ src/app/features/posts/PostsList.tsx
@@ -1,21 +1,23 @@
 import { useSelector } from "react-redux/es/hooks/useSelector";
-import { Box, Typography } from "@mui/material";
+import { Box, Paper, Stack, Typography } from "@mui/material";
 import { selectAllPosts } from "./postSlice";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
   const renderPosts = posts.map((post) => (
-    <Box component={"article"} key={post.id}>
-      <Typography variant={"h3"}>{post.title}</Typography>
+    <Paper variant="outlined" component={"article"} key={post.id} sx={{ p: 2 }}>
+      <Typography variant={"h4"}>{post.title}</Typography>
       <Typography variant="body1">{post.content.substring(0, 100)}</Typography>
-    </Box>
+    </Paper>
   ));
 
   return (
     <Box>
-      <Typography>PostsList</Typography>
-      {renderPosts}
+      <Typography variant={"h3"} my={2}>PostsList</Typography>
+      <Stack spacing={2} sx={{ maxWidth: "300px" }}>
+        {renderPosts}
+      </Stack>
     </Box>
   );
 };
