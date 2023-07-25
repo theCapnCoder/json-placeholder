4556c895a80a3ee2bf605d0ab4543a0c008bd97a
PavelM <myagkiypavel@gmail.com>
2023-07-25 15:17:24 +0300

    chore: AddPostForm

--- src/app/features/posts/AddPostForm.tsx
+++ src/app/features/posts/AddPostForm.tsx
@@ -1,12 +1,15 @@
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, MenuItem, Stack, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
 import { addPost } from "./postSlice";
+import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const users = useSelector(selectAllUsers);
 
   const dispatch = useDispatch();
 
@@ -18,24 +21,38 @@ const AddPostForm = () => {
     setContent(e.target.value);
   };
 
+  const onAuthorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+  };
+
   const onSavePostClicked = (e: React.FormEvent) => {
     e.preventDefault();
 
     if (title && content) {
-      dispatch(addPost(title, content));
+      dispatch(addPost(title, content, userId));
       setTitle("");
       setContent("");
+      setUserId("");
     }
   };
+
+  const usersOptions = users.map((user) => (
+    <MenuItem key={user.id} value={user.id}>
+      {user.name}
+    </MenuItem>
+  ));
+
+  const canSave = [title, content, userId].every(Boolean);
 
   return (
     <Box>
       <Typography variant="h3">AddPost</Typography>
-      <form onClick={onSavePostClicked}>
+      <form onSubmit={onSavePostClicked}>
         <Stack spacing={2} sx={{ maxWidth: "300px" }}>
           <TextField type="text" value={title} onChange={onTitleChange} />
+          <TextField select value={userId} onChange={onAuthorChange}>{usersOptions}</TextField>
           <TextField type="text" value={content} onChange={onContentChange} />
-          <Button variant="contained" type="submit">
+          <Button variant="contained" type="submit" disabled={!canSave}>
             Save Post
           </Button>
         </Stack>
