4e50f2a19a6b4ca0b24c3a018c07e67ed915fd6a
PavelM <myagkiypavel@gmail.com>
2023-07-25 18:52:07 +0300

    chore: addPostFormApi

--- src/app/features/postsAPI/AddPostFormApi.tsx
+++ src/app/features/postsAPI/AddPostFormApi.tsx
@@ -1,17 +1,28 @@
-import { Box, Button, MenuItem, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  MenuItem,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addPost } from "./postSlice";
 import { selectAllApiUsers } from "../users/usersApiSlice";
+import { addNewPost } from "./postSlice";
+import { AppDispatch } from "../../store";
 
 const AddPostFormApi = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [userId, setUserId] = useState("");
+  const [addRequestStatus, setAddRequestStatus] = useState<
+    "idle" | "pending" | "succeeded" | "failed"
+  >("idle");
 
   const users = useSelector(selectAllApiUsers);
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
   const onTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
@@ -25,14 +36,25 @@ const AddPostFormApi = () => {
     setUserId(e.target.value);
   };
 
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === "idle";
+
   const onSavePostClicked = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (title && content) {
-      dispatch(addPost(title, content, userId));
-      setTitle("");
-      setContent("");
-      setUserId("");
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
+
+        setTitle("");
+        setContent("");
+        setUserId("");
+      } catch (err) {
+        console.log("Failed to save the post", err);
+      } finally {
+        setAddRequestStatus("idle");
+      }
     }
   };
 
@@ -42,15 +64,15 @@ const AddPostFormApi = () => {
     </MenuItem>
   ));
 
-  const canSave = [title, content, userId].every(Boolean);
-
   return (
     <Box>
       <Typography variant="h3">AddPost</Typography>
       <form onSubmit={onSavePostClicked}>
         <Stack spacing={2} sx={{ maxWidth: "300px" }}>
           <TextField type="text" value={title} onChange={onTitleChange} />
-          <TextField select value={userId} onChange={onAuthorChange}>{usersOptions}</TextField>
+          <TextField select value={userId} onChange={onAuthorChange}>
+            {usersOptions}
+          </TextField>
           <TextField type="text" value={content} onChange={onContentChange} />
           <Button variant="contained" type="submit" disabled={!canSave}>
             Save Post
