398631492811594d478c074b13f4657dbff183c9
PavelM <myagkiypavel@gmail.com>
2023-07-25 15:55:34 +0300

    chore: PostList

--- src/app/features/posts/PostsList.tsx
+++ src/app/features/posts/PostsList.tsx
@@ -4,17 +4,23 @@ import { selectAllPosts } from "./postSlice";
 import AddPostForm from "./AddPostForm";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderPosts = posts.map((post) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderPosts = orderedPosts.map((post) => (
     <Paper variant="outlined" component={"article"} key={post.id} sx={{ p: 2 }}>
       <Typography variant={"h4"}>{post.title}</Typography>
       <Typography variant="body1">{post.content.substring(0, 100)}</Typography>
       <Typography variant="caption">
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
+        <ReactionButtons post={post} />
       </Typography>
     </Paper>
   ));
