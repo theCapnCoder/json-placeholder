074aa1e1c68d8e3a4fa41b240b8087ffd017ef2e
PavelM <myagkiypavel@gmail.com>
2023-07-25 15:17:48 +0300

    feature: add PostAuthor

---
+++ src/app/features/posts/PostAuthor.tsx
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux";
+import { selectAllUsers } from "../users/usersSlice";
+import { Typography } from "@mui/material";
+
+const PostAuthor = ({ userId }: { userId: number | undefined }) => {
+  const users = useSelector(selectAllUsers);
+  console.log(userId);
+
+  const author = users.find((user) => user.id === userId);
+  return (
+    <Typography variant="caption" component={"span"}>
+      {author ? author.name : "Unknown author"}
+    </Typography>
+  );
+};
+
+export default PostAuthor;
