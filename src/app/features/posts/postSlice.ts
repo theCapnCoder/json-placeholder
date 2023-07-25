06e59b81fd3bf98740a0af91f7ff0f9dee1c2b95
PavelM <myagkiypavel@gmail.com>
2023-07-25 15:55:26 +0300

    chore: add reactionAdded reducer

--- src/app/features/posts/postSlice.ts
+++ src/app/features/posts/postSlice.ts
@@ -2,12 +2,21 @@ import { PayloadAction, createSlice, nanoid } from '@reduxjs/toolkit'
 import { RootState } from '../../store'
 import { sub } from 'date-fns'
 
-type Post = {
+export type Reactions = {
+  thumbsUp: number
+  wow: number
+  heart: number
+  rocket: number
+  coffee: number
+}
+
+export type Post = {
   id: number
   title: string
   content: string
-  date?: string
+  date: string
   userId?: number
+  reactions: Record<keyof Reactions, number>
 }
 
 const initialState: Post[] = [
@@ -16,6 +25,13 @@ const initialState: Post[] = [
     title: 'First post',
     content: 'First post content',
     date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0
+    }
   },
   // { id: 2, title: 'Second post', content: 'Second post content', },
   {
@@ -23,6 +39,13 @@ const initialState: Post[] = [
     title: 'Third post',
     content: ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est aut iure accusantium aspernatur reiciendis suscipit eveniet soluta non aperiam amet? Alias repellendus ab temporibus nostrum fuga et harum, voluptates saepe deleniti commodi. Minima placeat, necessitatibus quaerat voluptates labore inventore illo excepturi consequuntur saepe doloribus reprehenderit. Beatae dolores ipsum deserunt dignissimos dicta iusto maxime a perferendis nemo sed dolorem amet molestiae vitae possimus, nesciunt saepe quos provident maiores incidunt laborum!  Atque quia non placeat unde aliquid error ea ipsam dolor sunt fugit, vero consectetur praesentium aliquam ullam officia repudiandae impedit quidem ut! Eveniet, explicabo voluptates, rerum officia dolore maiores magnam assumenda sint eos eaque eius sequi ad fugiat alias accusamus harum maxime voluptas! Quas corrupti inventore amet, ipsum a odio porro corporis, veniam iure deserunt alias saepe placeat, facilis dolores? Et qui laudantium neque, quo aliquam deleniti labore doloribus, nihil consequatur, atque illo magnam inventore vero rerum similique tenetur quisquam voluptatem temporibus velit? Eaque ipsa libero illum impedit odit facilis soluta iure perferendis voluptate consequatur ad quibusdam velit corrupti adipisci, praesentium ullam quidem repellendus cupiditate similique. Accusamus ab unde cupiditate doloribus dolores tempora dolorum, maiores soluta, optio, alias quo culpa temporibus.',
     date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0
+    }
   },
 ]
 
@@ -42,8 +65,22 @@ const postsSlice = createSlice({
             content,
             date: new Date().toISOString(),
             userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0
+            }
           },
         }
+      }
+    },
+    reactionAdded: (state, action: PayloadAction<{ postId: number, reaction: keyof Reactions }>) => {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++  
       }
     }
   }
@@ -51,6 +88,6 @@ const postsSlice = createSlice({
 
 export const selectAllPosts = (state: RootState) => state.posts
 
-export const { addPost } = postsSlice.actions
+export const { addPost, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer;
