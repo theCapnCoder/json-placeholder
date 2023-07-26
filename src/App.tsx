f07512cf0db7a4cd6e9c310562091087c75f0e50
PavelM <myagkiypavel@gmail.com>
2023-07-26 14:05:27 +0300

    chore: App

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import { Menu } from "./components/menu";
 function App() {
   return (
     <Box>
-      <Typography component={"h1"} variant="h3" sx={{ textAlign: "center" }}>
+      <Typography
+        component={"h1"}
+        variant="h3"
+        sx={{ textAlign: "center", mb: 4 }}
+      >
         JSON placeholder
       </Typography>
       <Stack direction={"row"} spacing={2}>
